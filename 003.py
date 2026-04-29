a3b0da47eff3e99307cb67750cb970c34abfceb2
fengloop186 <socbel25@gmail.com>
2026-04-29 21:58:14 +0700

    003

--- 003.py
+++ 003.py
@@ -1,14 +1,10 @@
-#Hiển thị từ cách nhau bởi ký tự "--" ra màn hình
-"""Kiến thức cần có:
-Hàm input() và hàm print()
-Tham số sep của hàm print()
-Biến và kiểu dữ liệu"""
-#Code tự làm
-ten = input()
-print('Xin','chao!','Toi','ten','la',ten, sep = "--")
-#Code đối chiếu
-#Nhap gia tri {Ten cua ban}
-ten = input()
-#Xuat cau voi dinh dang theo yeu cau de bai
-#Tham so sep='--' giup ngan cach cac tu boi '--'
-print('Xin', 'chao!', 'Toi', 'ten', 'la', ten, sep='--')
+#Display the words separated by "--" on the screen
+#Practice version
+name = input()
+print('Hello', 'my', 'name', 'is','Feng', sep = '--')
+#Improved version
+#Enter value {Your name}
+name = input()
+#Output the sentence in the format required by the problem
+#The parameter sep= '--' helps separate the words with '--'
+print('Hello', 'my', 'name', 'is', 'Feng', sep = '--')
