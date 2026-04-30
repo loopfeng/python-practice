1b7ec1ccebcda619f3898cd7dbcc4f0198cdc59c
fengloop186 <socbel25@gmail.com>
2026-04-30 08:36:50 +0700

    003

--- 001.py
+++ 001.py
@@ -1,12 +1,12 @@
-#01 Calculate and display the sum of any two number integers
+#01 Calculate and display the sum of any two integers
 
-#Practice version
-num1 = int(input("Enter the first number : "))
-num2 = int(input("Enter the second number : "))
+#Pactice version
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
 total = num1 + num2
-print("The sum of two number is:", total)
+print('The sum of two number is:', total)
 
 #Improved version
-num1 = int(input("Enter the first number: "))# Convert integer
-num2 = int(input("Enter the second number: "))# Convert integer
-print("The sum of two number is:", num1 + num2)
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+print('The sum of two number is:', num1 + num2)
